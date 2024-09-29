f68ac3171681c0583cb005f429116e9dd7cd0575
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-09-29 09:11:19 +0300

    Update NativeWindow.hx

--- src/openfl/display/NativeWindow.hx
+++ src/openfl/display/NativeWindow.hx
@@ -826,7 +826,7 @@ class NativeWindow extends EventDispatcher
 		are smaller than the new minimum size.
 
 		The `minSize` restriction is enforced for window resizing operations
-		invoked both through haxe code and through the operating system.
+		invoked both through Haxe code and through the operating system.
 
 		Note: The width and height of any displayed system chrome may make it
 		impossible to set a window as small as the specified minimum size.
@@ -1095,45 +1095,53 @@ class NativeWindow extends EventDispatcher
 		return __ownedWindows.copy();
 	}
 
-	@:noCompletion private inline function window_onActivate():Void
+	@:noCompletion private function window_onActivate():Void
 	{
-		window_onFocusIn();
+		if (!__active)
+		{
+			window_onFocusIn();
+		}
 	}
 
-	@:noCompletion private inline function window_onDeactivate():Void
+	@:noCompletion private function window_onDeactivate():Void
 	{
-		window_onFocusOut();
+		if (__active)
+		{
+			window_onFocusOut();
+		}
 	}
 
 	@:noCompletion private function window_onFocusIn():Void
 	{
-		if (!__active)
+		if (__active)
 		{
-			__active = true;
-			NativeApplication.nativeApplication.__activeWindow = this;
-			dispatchEvent(new Event(Event.ACTIVATE, false, false));
-			// TODO: dispatch only when the previously focused window was from
-			// a different application
-			NativeApplication.nativeApplication.dispatchEvent(new Event(Event.ACTIVATE, false, false));
+			return;
 		}
+
+		__active = true;
+		NativeApplication.nativeApplication.__activeWindow = this;
+		dispatchEvent(new Event(Event.ACTIVATE, false, false));
+		// TODO: dispatch only when the previously focused window was from
+		// a different application
+		NativeApplication.nativeApplication.dispatchEvent(new Event(Event.ACTIVATE, false, false));
 	}
 
 	@:noCompletion private function window_onFocusOut():Void
 	{
-		if (__active)
+		if (!__active)
 		{
-			__active = false;
-
-			if (NativeApplication.nativeApplication.__activeWindow == this)
-			{
-				NativeApplication.nativeApplication.__activeWindow = null;
-				// TODO: dispatch only when the next focused window isn't a part of
-				// this application
-				NativeApplication.nativeApplication.dispatchEvent(new Event(Event.DEACTIVATE, false, false));
-			}
-
-			dispatchEvent(new Event(Event.DEACTIVATE, false, false));
+			return;
 		}
+
+		__active = false;
+		if (NativeApplication.nativeApplication.__activeWindow == this)
+		{
+			NativeApplication.nativeApplication.__activeWindow = null;
+			// TODO: dispatch only when the next focused window isn't a part of
+			// this application
+			NativeApplication.nativeApplication.dispatchEvent(new Event(Event.DEACTIVATE, false, false));
+		}
+		dispatchEvent(new Event(Event.DEACTIVATE, false, false));
 	}
 
 	@:noCompletion private function window_onMove(x:Float, y:Float):Void
